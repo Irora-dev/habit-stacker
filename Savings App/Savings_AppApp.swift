491f903a859e1cf1b644402658df5c82c1f9bd34
Colby Mort <thetalesofephemera@gmail.com>
2025-12-30 11:10:17 +0100

    Initial Commit

---
+++ Savings App/Savings_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Savings_AppApp.swift
+//  Savings App
+//
+//  Created by Colby Mort on 30/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct Savings_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Savings AppTests/Savings_AppTests.swift
@@ -0,0 +1,16 @@
+//
+//  Savings_AppTests.swift
+//  Savings AppTests
+//
+//  Created by Colby Mort on 30/12/2025.
+//
+
+import Testing
+
+struct Savings_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
